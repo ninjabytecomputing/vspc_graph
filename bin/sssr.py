e39e405e8aca4b0a90430806532525bed98e7220
ninjabytecomputing <ninjabytecomputing@gmail.com>
2020-05-20 03:00:40 -0700

    Parallel invoking of C++ code

--- bin/sssr.py
+++ bin/sssr.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import os
+import itertools
+import multiprocessing as mp
 import shlex
 import subprocess
 import sys
@@ -8,15 +10,20 @@ import sys
 EXE = './release/src/main'
 
 
+def init(l):
+    global lock
+    lock = l
+
+
 def main(files):
-    # Filter out any files with 'cycle' as a substring in the base filename
-    files = [f for f in files if 'cycle' not in os.path.basename(f)]
+    # Filter out any files with 'cycles' as a substring in the base filename
+    files = [f for f in files if 'cycles' not in os.path.basename(f)]
 
     notCSVFiles      = []
     dneFiles         = []
     notGraphFiles    = []
     noOverwriteFiles = []
-    fileMap          = {}
+    ioFiles          = []
 
     for f in files:
         if not f.endswith('.csv'):
@@ -30,27 +37,43 @@ def main(files):
                     notGraphFiles.append(f)
                 else:
                     outFile = os.path.dirname(f) + '/'
-                    outFile += bname.replace('graph', 'cycle')
+                    outFile += bname.replace('graph', 'cycles')
                     if os.path.exists(outFile):
                         resp = input(outFile + " already exists. Overwrite [y/n]? ")
                         if resp == 'y' or resp == 'Y':
-                            fileMap[f] = outFile
+                            ioFiles.append((f, outFile))
                         else:
                             noOverwriteFiles.append(f)
                     else:
-                        fileMap[f] = outFile
+                        ioFiles.append((f, outFile))
 
     print('+-----------------------------------------------------------+')
     print('|                          Results                          |')
     print('+-----------------------------------------------------------+')
-    for inF, outF in fileMap.items():
-        cmd = ' '.join([EXE, inF, outF])
-        # subprocess.check_call(shlex.split(cmd))
+
+    # Initialize lock for printing in parallel
+    lock = mp.Lock()
+    # Initialize pool for launching tasks asynchronously
+    pool = mp.Pool(initializer = init,
+                   initargs = (lock,),
+                   processes = mp.cpu_count())
+    pool.map_async(processFile, ioFiles)
+    pool.close()
+    pool.join()
 
     print('+-----------------------------------------------------------+')
     print('|                          Skipped                          |')
     print('+-----------------------------------------------------------+')
     # Print the skipped files nicely
+
+
+def processFile(ioPair):
+    inFile, outFile = ioPair
+    cmd = ' '.join([EXE, inFile, outFile])
+    subprocess.check_call(shlex.split(cmd))
+    lock.acquire()
+    print(inFile + ' -> ' + outFile)
+    lock.release()
 
 
 if __name__ == '__main__':
--- src/main.cpp
+++ src/main.cpp
@@ -39,35 +39,16 @@ int main(int argc, char* argv[]) {
     // Construct SSSR object
     vspc::SSSR op(graph);
 
-    // std::cout << "+------------------------------------------------------------------+\n";
-    // std::cout << "|                            Input data                            |\n";
-    // std::cout << "+------------------------------------------------------------------+\n";
-    // std::cout << "  Input file       : " << inputName << "\n";
-    // std::cout << "  Number of nodes  : " << graph.numNodes() << "\n";
-    // std::cout << "  Node index range : "
-    //           << "[" << graph.minNode() << ", " << graph.maxNode() << "]\n";
-    // std::cout << "  Number of edges  : " << graph.numEdges() << "\n\n";
-
     // Find SSSR and return the vector of cycles
     const auto& cycles = op.run();
 
-    // std::cout << "+------------------------------------------------------------------+\n";
-    // std::cout << "|                              Results                             |\n";
-    // std::cout << "+------------------------------------------------------------------+\n";
-    // std::cout << "  Ouptut file      : " << outputName << "\n";
-    // std::cout << "  Number of cycles : " << cycles.size() << "\n";
-
     {
         std::ofstream fout(outputName);
-
         for (size_t i = 0, n = cycles.size(); i < n; ++i) {
-            // Sanity check!
-            if (cycles[i].numEdges() != cycles[i].numNodes()) {
-                std::cout << "Cycle " << i << " failed the sanity check - skipping\n";
-            } else {
-                fout << convertGraphToPath(cycles[i]).strAsCSV() << "\n";
-            }
+            fout << convertGraphToPath(cycles[i]).strAsCSV() << "\n";
         }
     }
+
+    return 0;
 }
 
